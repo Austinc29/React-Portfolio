ca2cd0ebddcdcaa350d867063c479fcb8afd8ead
Austin <aac729@live.com>
2021-04-28 16:02:44 -0500

    1st commit

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -14,8 +14,7 @@ export default class ContactUs extends Component {
           <div className="row">
             <aside className="eigth columns footer-widgets">
               <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
+                <h4><a href="mailto:austincu29@gmail.com">Email Me</a>
                 </h4>
               </div>
             </aside>
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,74 +1,68 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "name": "Austin Cummings",
+    "role": "Full stack Cloud Developer and MERN Developer",
+    "roleDescription": " I like dabbling in various parts of Frontend development and like to learn about new technologies, playing around with Github repos and creating my own projects or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/austin-cu/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/Austinc29",
           "className":"fa fa-github"
         },
         {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
+          "name":"twitter",
+          "url":"https://twitter.com/Devv_Austin",
           "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"I am a self-taught web developer currently obtaining my Certification in Full Stack Cloud Development from Coursera/IBM specializing in their cloud technologies and the MERN technology stack. After I plan on obtaining my certification with Google in their Google Cloud programs. ",
+    "address":"USA",
+    "email":"austincu29@gmail.com",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
+        "UniversityName":"IBM Full Stack Cloud Developer Professional Certification",
+        "specialization":"Full Stack Web Development, MongoDB, Express.js, React.js, Node.js, Cloud Development, React Native Applications",
+        "MonthOfPassing":"July",
+        "YearOfPassing":"2021",
         "Achievements":"Some Achievements"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Minooka Community High School",
+        "specialization": "",
+        "MonthOfPassing":"Gradution May",
+        "YearOfPassing":"2017",
+        "Achievements":""
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"The Home Depot",
+        "specialization":"Lumber Sales Associate",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Current",
+        "Achievements":""
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
+        "CompanyName":"NFI ",
+        "specialization":"Forklift Operator",
         "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2020",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Expert Electrical Contractors",
+        "specialization":"Construction Laborer",
+        "MonthOfLeaving":"Aug",
         "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "Achievements":""
       }
     ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
+    "skillsDescription":"HTML5 | CSS3 | Javascript | IBM Cloud | Cloud Development | MongoDB | Express.js | React.js | Node.js | SQL | Ruby | React Native | Github | Git ",
     "portfolio":[
       {
         "name":"project1",
@@ -91,16 +85,6 @@ let resumeData = {
         "imgurl":"images/portfolio/phone.jpg"
       }
     ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
   }
   
   export default resumeData
